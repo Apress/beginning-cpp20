28d96b753ce81f948d6460c804af6bfaf54f638a
peter_vanweert <peter.vanweert@gmail.com>
2020-10-04 16:59:26 +0200

    Add source code for Chapter 1

---
+++ Examples/NoModules/Chapter 01/Ex1_01.cpp
@@ -0,0 +1,13 @@
+// A complete C++ program
+#include <iostream>
+
+int main()
+{
+  int answer {42};              // Defines answer with value 42
+
+  std::cout << "The answer to life, the universe, and everything is "
+            << answer
+            << std::endl;
+
+  return 0;
+}
---
+++ Exercises/NoModules/Chapter 01/Soln1_02.cpp
@@ -0,0 +1,11 @@
+// Exercise 1-2. Write your name and age on successive lines.
+// There are several possibilities. You can do it in one statement for example.
+// You could also output '\n' to go to a new line.
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Phil McCavity" << std::endl;       // Name
+  std::cout << "Age: 88" << std::endl;             // and age
+}
