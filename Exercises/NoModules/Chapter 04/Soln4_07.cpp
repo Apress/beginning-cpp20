7f6c525dcddaa82ef0fa69db71c30a2f5dd95cae
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-09 17:04:48 +0200

    Chapter 04: convert to lower case as instructed

--- Exercises/NoModules/Chapter 04/Soln4_07.cpp
+++ Exercises/NoModules/Chapter 04/Soln4_07.cpp
@@ -14,23 +14,27 @@
 
 int main()
 {
-  char ch {};
+  char entered_letter {};
   std::cout << "Enter a letter: ";
-  std::cin >> ch;
+  std::cin >> entered_letter;
 
-  if (!std::isalpha(ch)) 
+  if (!std::isalpha(entered_letter)) 
   {
     std::cout << "That's not a letter!" << std::endl;
     return 1;
   }
 
+  // We'll need the lower case letter...
+  const auto lower_case_letter{ static_cast<char>(std::tolower(entered_letter)) };
+
   // Determine upper or lower case.
-  std::cout << "'" << ch << "' is " << 
-    (std::islower(ch) ? "lowercase" : "uppercase") << '.' << std::endl;
+  std::cout << "'" << entered_letter << "' is " << 
+    (std::islower(entered_letter) ? "lowercase" : "uppercase") << '.' << std::endl;
 
   // Determine whether it is a vowel or a consonant.
-  std::cout << "'" << ch << "' is a ";
-  switch(std::tolower(ch))
+  std::cout << "'" << entered_letter << "' is a ";
+
+  switch (lower_case_letter)
   {
     case 'a': case 'e': case 'i': case 'o': case 'u':
       std::cout << "vowel";
@@ -42,15 +46,15 @@ int main()
   std::cout << '.' << std::endl;
 
   // Output the character code as binary
-  std::cout << "The binary code for '" << ch << "' is "
-     << ((ch & 0b10000000)? 1 : 0) 
-	   << ((ch & 0b01000000)? 1 : 0)
-     << ((ch & 0b00100000)? 1 : 0) 
-	   << ((ch & 0b00010000)? 1 : 0)
-     << ((ch & 0b00001000)? 1 : 0) 
-	   << ((ch & 0b00000100)? 1 : 0)
-     << ((ch & 0b00000010)? 1 : 0) 
-	   << ((ch & 0b00000001)? 1 : 0)
+  std::cout << "The binary code for '" << lower_case_letter << "' is "
+     << ((lower_case_letter & 0b10000000)? 1 : 0) 
+     << ((lower_case_letter & 0b01000000)? 1 : 0)
+     << ((lower_case_letter & 0b00100000)? 1 : 0) 
+     << ((lower_case_letter & 0b00010000)? 1 : 0)
+     << ((lower_case_letter & 0b00001000)? 1 : 0) 
+     << ((lower_case_letter & 0b00000100)? 1 : 0)
+     << ((lower_case_letter & 0b00000010)? 1 : 0) 
+     << ((lower_case_letter & 0b00000001)? 1 : 0)
      << std::endl;
 
   return 0;
