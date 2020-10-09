50900743f2cd507557c98698004aa8d3d96ca24e
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-09 15:56:55 +0200

    Chapter 03: modernise solution to Exercise 4 (use std::format)

--- Exercises/NoModules/Chapter 03/Soln3_04.cpp
+++ Exercises/NoModules/Chapter 03/Soln3_04.cpp
@@ -6,44 +6,49 @@
 // and the fundamental type int counts at least 4 bytes (true for most modern systems).
 
 #include <iostream>
-#include <iomanip>
+#include <format>
 
 int main()
 {
   unsigned int packed {};
   unsigned char ch {};
-  std::cout << std::left << std::setw(26) << "Enter a character: ";
+  std::cout << std::format("{:26}", "Enter a character: ");
   std::cin >> ch;
   packed |= ch;
 
-  std::cout << std::setw(26) << "Enter a second character: ";
-  std::cin >> ch;
-  packed <<= 8;                                // Shift left 1 byte
-  packed |= ch;
-
-  std::cout << std::setw(26) << "Enter a third character: ";
+  std::cout << std::format("{:26}", "Enter a second character: ");
   std::cin >> ch;
   packed <<= 8;                                // Shift left 1 byte
   packed |= ch;
 
-  std::cout << std::setw(26) << "Enter a fourth character: ";
+  std::cout << std::format("{:26}", "Enter a third character: ");
   std::cin >> ch;
   packed <<= 8;                                // Shift left 1 byte
   packed |= ch;
 
-  std::cout << "The word containing 4 packed characters is " << std::right
-    << std::hex << std::showbase << std::internal << std::setfill('0') << packed << std::endl;
+  std::cout << std::format("{:26}", "Enter a fourth character: ");
+  std::cin >> ch;
+  packed <<= 8;                                // Shift left 1 byte
+  packed |= ch;
 
-  std::cout << std::setfill(' ');
+  std::cout << std::format("The word containing 4 packed characters is {:#0x}", packed) << std::endl;
 	
-  // Unpacking packed...
-  unsigned int mask {0x000000FF};              // Keep low order byte
-  ch = mask & packed;                          // Low order byte
-  std::cout << std::setw(4) << ch;
-  ch = mask & (packed >> 8);                   // 2nd byte
-  std::cout << std::setw(4) << ch;
-  ch = mask & (packed >> 16);                  // 3rd byte
-  std::cout << std::setw(4) << ch;
-  ch = mask & (packed >> 24);                  // 4th byte
-  std::cout << std::setw(4) << ch << std::endl;
+  std::cout << "Unpacking the characters again gives (low-order byte first): ";
+
+  // Unpacking packed... 
+  // (without the static_cast<char>() conversions the bytes would be written as numbers,
+  // which would be fine as well...)
+
+  unsigned int mask {0x00000FF};  // Keep low order byte 
+                                  // (could just use 0xFF as well, or, of course, 255)
+
+  ch = packed & mask;                    // Low order byte
+  std::cout << std::format("{:4}", static_cast<char>(ch));
+  ch = (packed >> 8) & mask;                   // 2nd byte
+  std::cout << std::format("{:4}", static_cast<char>(ch));
+  ch = (packed >> 16) & mask;                  // 3rd byte
+  std::cout << std::format("{:4}", static_cast<char>(ch));
+  ch = (packed >> 24) & mask;                  // 4th byte
+  std::cout << std::format("{:4}", static_cast<char>(ch)) 
+            << std::endl;
 }
