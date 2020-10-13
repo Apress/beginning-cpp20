1a2a80697f54460c9590a43e97a4c0fc54742b93
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-13 18:01:02 +0200

    Chapter 06: generate odd numbers as requested

--- Exercises/NoModules/Chapter 06/Soln6_02.cpp
+++ Exercises/NoModules/Chapter 06/Soln6_02.cpp
@@ -10,10 +10,10 @@ int main()
   const size_t n {50};
   size_t odds[n];
   for (size_t i {}; i < n; ++i)
-    odds[i] = i + 1;
+    odds[i] = i * 2 + 1;
 
   const size_t perline {10};
-  std::cout << "The numbers are:\n";
+  std::cout << "The " << n << " first odd numbers are:\n";
   
   size_t* traversal_pointer{ odds };
   for (size_t i {}; i < n; ++i)
@@ -23,7 +23,7 @@ int main()
       std::cout << std::endl;
   }
 
-  std::cout << "\nIn reverse order the numbers are:\n";
+  std::cout << "\nIn reverse order these numbers are:\n";
   for (size_t i {}; i < n; ++i)      // No need to reverse the manipulation of the loop counter now
   {
     std::cout << std::format("{:5}", *(--traversal_pointer)); // Use the pre-decrement operator to make sure the pointer is decremented
