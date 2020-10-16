a5ea98dc48b216e5ba55351b47d2f52d50338c44
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-16 17:33:37 +0200

    Chapter 07: redo exercise 7 such that it can cope with inputs such as "1 2 3#"

--- Exercises/NoModules/Chapter 07/Soln7_07.cpp
+++ Exercises/NoModules/Chapter 07/Soln7_07.cpp
@@ -1,43 +1,47 @@
 // Exercise 7-7
 // Practice string concatenation, looping over strings, and stoi()-like functions
 #include <iostream>
-#include <string> 
+#include <string>
 
 int main()
 {
-  std::string numbers;
-  
   std::cout << "Enter a sequence of numbers terminated by #:\n";
   
-  while (true)
-  {
-    std::string number;         // Read strings one by one
-    std::cin >> number;
-    if (number == "#") break;   // Stop indefinite loop once "#" is encountered
-    numbers += number + ' ';    // Concatenate all individual strings into one single string
-  }
+  // Read a long string containing any number of integers
+  std::string numbers;
+  std::getline(std::cin, numbers, '#');
 
   long sum {};
   
   size_t index {};
-  while (true)
+  while (true)  // An indefinite loop, stopped using a break statement.
   {
-    const size_t found{ numbers.find(' ', index) }; // Search for the next space
-    if (found == std::string::npos) break;          // Stop once we're at the end of the string
-    const size_t length{ found - index };           // To access substrings, we need the length of the string
-    const int i{ std::stoi(numbers.substr(index, length)) };
-    sum += i;
-    index += length + 1;                            // Increase the starting index (include the space)
+    // Note: this solution does not verify that in between the numbers there's only whitespace. 
+    // In real programs, such input validation may be appropriate...
+
+    // Search for the start of the next (signed!) number
+    const size_t start{ numbers.find_first_of("-0123456789", index) }; 
+    if (start == std::string::npos) 
+      break;  // Stop once there's no numbers left
+
+    // Search for the end of the number
+    size_t end{ numbers.find_first_not_of("0123456789", start + 1) };
+    if (end == std::string::npos)
+      end = numbers.length();
+
+    const size_t length{ end - start };   // To access substrings, we need the length of the string
+    const auto substring{ numbers.substr(start, length) };
+
+    // Just in case: skip minus signs not followed by a number (would otherwise crash...)
+    if (substring != "-")
+    {
+      const int number{ std::stoi(substring) };
+
+      sum += number;
+    }
+    
+    index = end;   // Advance to the next input (if any)
   }
     
   std::cout << "The sum of the numbers you entered is: " << sum << std::endl;
-  
-  /*
-  Note: another option would be a loop of the form
-  
-  for (size_t index {}; index < numbers.size(); )
-  {
-    // ... same as before, but the line that checks for npos and breaks can be omitted
-  }
-  */
 }
