2d585f394a2baf7b84e7e5107d0b36f34cea9853
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-18 18:16:12 +0200

    Chapter 16: remove copy prevention (Exercise 18-3 is all about adding these---sadly...)

--- Exercises/NoModules/Chapter 16/Soln16_06/DB_RAII.h
+++ Exercises/NoModules/Chapter 16/Soln16_06/DB_RAII.h
@@ -4,10 +4,6 @@
   In this solution we have the RAII classes accept the resource handle in their constructor.
   Alternatively, you could also acquire the resources inside the constructors of the RAII class.
   For instance: you could call db_connect() from within the DBConnectionRAII() contructor.
-
-  When creating RAII classes, it is typically crucial they cannot be copied
-  (otherwise multiple objects would be releasing the same resource, 
-   which is typically not allowed). To accomplish this, you delete both copy members.
 */
 
 #ifndef DB_RAII_H
@@ -34,10 +30,6 @@ public:
     }
   }
 
-  // Prevent copying by deleting both copy members
-  DBConnectionRAII(const DBConnectionRAII& other) = delete;
-  DBConnectionRAII& operator=(const DBConnectionRAII& other) = delete;
-
   // Implicit type conversion to the underlying resource handle.
   // Note: many RAII types will use a get() function instead 
   // (unique_ptr / shared_ptr, for instance, do this)
@@ -63,10 +55,6 @@ public:
       db_free_result(m_result);
     }
   }
-
-  // Prevent copying by deleting both copy members
-  DBQueryResultRAII(const DBQueryResultRAII& other) = delete;
-  DBQueryResultRAII& operator=(const DBQueryResultRAII& other) = delete;
 
   // Implicit type conversion to the underlying resource handle.
   // Note: many RAII types will use a get() function instead 
