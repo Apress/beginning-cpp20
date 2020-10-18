8d3d2c5440f37d85fa1d985260722e0966ea7c52
Peter Van Weert <peter.vanweert@gmail.com>
2020-10-18 11:26:47 +0200

    Chapter 12: use more meaningful parameter names

--- Exercises/NoModules/Chapter 12/Soln12_05/Integer.h
+++ Exercises/NoModules/Chapter 12/Soln12_05/Integer.h
@@ -4,12 +4,12 @@
 class Integer
 {
 public:
-  Integer(int m = 0);                      // Contructor with given value
+  Integer(int value = 0);                  // Contructor with given value
   Integer(const Integer& obj);             // Copy constructor
   ~Integer();                              // Destructor
 
   int getValue() const { return m_value; }
-  void setValue(int m) { m_value = m; }
+  void setValue(int value) { m_value = value; }
 
   int compare(const Integer& obj) const;   // Compare function with reference parameter
 
