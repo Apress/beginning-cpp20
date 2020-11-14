4b71f9a86ec666172c3942437938fedc7896b616
Peter Van Weert <peter.vanweert@gmail.com>
2020-11-14 10:34:00 +0100

    Appendix A: fix typo

--- Exercises/NoModules/Appendix A/SolnA_07A/PrintThat.h
+++ Exercises/NoModules/Appendix A/SolnA_07A/PrintThat.h
@@ -1,6 +1,6 @@
 // Note: #include guards are strictly speaking optional for headers
 // that contain only function prototypes, 
-// but convention dictates you add the regardless.
+// but convention dictates you add them regardless.
 #ifndef PRINT_THAT_H
 #define PRINT_THAT_H
 
--- Exercises/NoModules/Appendix A/SolnA_07A/PrintThis.h
+++ Exercises/NoModules/Appendix A/SolnA_07A/PrintThis.h
@@ -1,6 +1,6 @@
 // Note: #include guards are strictly speaking optional for headers
 // that contain only function prototypes, 
-// but convention dictates you add the regardless.
+// but convention dictates you add them regardless.
 #ifndef PRINT_THIS_H
 #define PRINT_THIS_H
 
